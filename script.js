a0fed08fcd511c6ceb85b98927beb6bb0ab2e566
Vitor Volpato <vitorvolpe14@gmail.com>
2026-09-18 14:07:55 -0300

    corrigir CORS e checkout

--- script.js
+++ script.js
@@ -1275,7 +1275,7 @@ function checkout() {
 
 
     fetch(
-        "https://monte-site-tfjk.onrender.com/api/criar-checkout",
+        "https://monte-site-itjk.onrender.com/api/criar-checkout",
         {
 
             method: "POST",
