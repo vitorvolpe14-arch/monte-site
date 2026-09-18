92a6aa61196ef05c6054e3459ce3823d3fce6d99
Vitor Volpato <vitorvolpe14@gmail.com>
2026-09-18 14:19:41 -0300

    corrige Cors do checkout

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,13 @@ require("dotenv").config();
 const app = express();
 app.use(express.static(path.join(__dirname, "..")));
 const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
+    origin: "https://monte-site-itjk.onrender.com",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"]
 };
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
